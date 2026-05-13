13ce675fbf36eb1dce6409e4ba13a8619e9d1a10
topdv-group <topdvgroup@gmail.com>
2026-05-13 17:06:29 +0200

    updated the fetch to meet server its deployed on instead of local server line 102

--- NO_STRUCTURE/login.js
+++ NO_STRUCTURE/login.js
@@ -99,7 +99,7 @@ loginButton.addEventListener('click', async () => {
   showLoading(true, "Verifying credentials...");
 
   try {
-    const response = await fetch('http://127.0.0.1:5000/api/login', {
+    const response = await fetch('/api/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
